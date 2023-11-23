cd92c04c6b73d313a2b275939819343a5587a2f8
demo <azureclassessept@gmail.com>
2023-11-23 04:00:19 +0000

    Updated variables.tf

--- Terraform/modules/env/variables.tf
+++ Terraform/modules/env/variables.tf
@@ -1,7 +1,7 @@
 variable "rg_name" {
   type = string
   validation {
-    condition = length(var.rg_name) > 5 && substr(var.rg_name,0,4) == "dev-"
+    condition = length(var.rg_name) > 5 //&& substr(var.rg_name,0,4) == "dev-"
     error_message = "it was not having length of 6 min or environment name is not in the start"
   }
 }
