f3af252fb3856ac8f9da43f47274d37c6fed2e2a
demo <azureclassessept@gmail.com>
2023-11-23 03:35:49 +0000

    Updated providers.tf

--- Terraform/modules/env/providers.tf
+++ Terraform/modules/env/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = {{ terraformrg }}
+    storage_account_name = {{ terraformstorage }}
+    container_name = {{ container }}
+    key = {{ statefilename }}
+  }
 }
 
 provider "azurerm" {
